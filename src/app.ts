3545e84053cc9165cf76dd09e7f0fcbd4f26abb7
daniv <danivego2015@gmail.com>
2025-06-19 12:27:58 -0500

    Add a index route to the API

--- src/app.ts
+++ src/app.ts
@@ -11,6 +11,15 @@ const app = express();
 app.use(cors()); // Solves cross-origin issues
 app.use(express.json()); // Parses JSON bodies
 
+// Main route - Welcome message
+app.get("/", (req: Request, res: Response) => {
+  res.status(200).json({
+    message: "Welcome to the To-Do API",
+    version: "1.0.0",
+    author: "Daniel Velasco (daniv)",
+  });
+});
+
 // Uses the routes from tasks.routes.ts
 app.use("/tasks", tasksRoutes);
 
